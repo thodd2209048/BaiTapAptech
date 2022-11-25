8e445369f4215ef9866851d84c919c8d7d783eff
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-11-25 17:36:34 +0700

    update

---
+++ html/20221125 lab6/main.js
@@ -0,0 +1,48 @@
+/*var username = document.getElementById('username');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+let errorMsg = document.getElementsByClassName('error'),
+  successIcon = document.getElementsByClassName('success-icon'),
+  failureIcon = document.getElementsByClassName('failure-icon');*/
+
+let id = (id) => document.getElementById(id); //tao ham id co tac dung lay id
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+  password = id("password"),
+  email = id("email"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+//let form = document.getElementsByTagName("form"); Co can su dụng lenh khai bao này không
+
+form.addEventListener("submit", (e) => {  //Nếu chỉ có 1 tag <form> thì có thể sử dụng tên tag để thay cho thẻ đó
+  e.preventDefault();
+ /*form submit la dong goi ca form gui di nen se xay ra ca 3 su kiện
+ Đây gọi là callback() trong js*/
+ engine(username,0,"Username cannot be blank!");
+ engine(email,1,"Username cannot be blank!");
+ engine(password,2,"Username cannot be blank!");
+
+
+});
+
+let engine = (id, serial, message) => {
+  if(id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    successIcon[serial].style.opacity = "0";
+    failureIcon[serial].style.opacity = "1";
+    id.style.border = "2px solid red";
+  } else {
+    successIcon[serial].style.opacity = "1";
+    failureIcon[serial].style.opacity = "0";
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+  }
+};
+
+
+
+
