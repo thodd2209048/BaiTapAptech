8cd84dad1d964729e57f8c617e315bb611a639af
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-12-07 15:23:10 +0700

    AngularForm

---
+++ Angular/20221207ValidationForm/js/app2.js
@@ -0,0 +1,12 @@
+// Create angular app
+
+var validationApp = angular.module('validationApp',[]);
+
+// Create angular controller
+validationApp.controller('mainController',function ($scope){
+  $scope.submitForm = function (isValid) {
+    if(isValid) {
+      alert('form is Valid');
+    }
+  };
+});
