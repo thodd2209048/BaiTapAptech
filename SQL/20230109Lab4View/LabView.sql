1f1bc524252fa4366f278e37efe06e255aad302c
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-09 16:49:56 +0700

    update

--- SQL/20230109Lab4View/LabView.sql
+++ SQL/20230109Lab4View/LabView.sql
@@ -1,3 +1,35 @@
 CREATE DATABASE Lab11
 GO
+
 USE Lab11
+GO
+
+CREATE VIEW ProductList
+AS
+ SELECT ProductID, Name FROM AdventureWorks2019.Production.Product
+GO
+
+SELECT * FROM ProductListGO
+
+USE AdventureWorks2019
+GO
+
+EXEC sp_help 'Sales.Customer'
+GO
+
+SELECT * FROM Sales.Customer
+GO
+
+CREATE VIEW KH2018
+AS SELECT FirstName, MiddleName, LastName, ssoh.OrderDate FROM Person.Person pp
+	INNER JOIN Sales.Customer sc ON pp.BusinessEntityID = sc.PersonID
+	INNER JOIN Sales.SalesOrderHeader ssoh ON ssoh.CustomerID = sc.CustomerID
+	GROUP BY FirstName, MiddleName, LastName
+WHERE YEAR(ssoh.OrderDate) = 2014
+GO
+
+Drop view KH2018
+GO
+
+SELECT * FROM KH2018
+GO
