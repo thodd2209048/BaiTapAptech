a0c49a4e5824e26acf705278ff9460e8ccf8112a
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-11 15:12:03 +0700

    update

---
+++ SQL/20230111SubQueryB10/SQLQueryB10.sql
@@ -0,0 +1,103 @@
+IF EXISTS (SELECT * FROM sys.databases WHERE NAME = 'B10Subquery')
+	DROP DATABASE B10Subquery
+GO
+
+CREATE DATABASE B10Subquery
+GO
+
+USE B10Subquery
+GO
+
+CREATE TABLE Student(
+	SID INT PRIMARY KEY,
+	S_FName VARCHAR(20) NOT NULL,
+	S_LName VARCHAR(30) NOT NULL
+)
+GO
+
+CREATE TABLE Course(
+	CID INT PRIMARY KEY,
+	C_Name VARCHAR(30) NOT NULL,
+)
+GO
+
+CREATE TABLE Course_Grades(
+	CGID INT PRIMARY KEY,
+	Semester CHAR(4) NOT NULL,
+	CID_CG	INT NOT NULL,
+	SID_CG INT NOT NULL,
+	Grade CHAR(2) NOT NULL,
+	CONSTRAINT CID_CG_FK FOREIGN KEY (CID_CG) REFERENCES Course(CID) ON DELETE CASCADE,
+	CONSTRAINT SID_CG_FK FOREIGN KEY (SID_CG) REFERENCES Student(SID) ON DELETE CASCADE
+)
+GO
+
+INSERT INTO Student (SID, S_FName, S_LName) VALUES (12345, 'Chris', 'Rock');
+INSERT INTO Student (SID, S_FName, S_LName) VALUES (23456, 'Chris', 'Farley');
+INSERT INTO Student (SID, S_FName, S_LName) VALUES (34567, 'David', 'Spade');
+INSERT INTO Student (SID, S_FName, S_LName) VALUES (45678, 'Liz', 'Lemon');
+INSERT INTO Student (SID, S_FName, S_LName) VALUES (56789, 'Jack', 'Donaghy');
+GO
+
+INSERT INTO Course (CID, C_Name) VALUES (101001, 'Intro to Computers');
+INSERT INTO Course (CID, C_Name) VALUES (101002, 'Programming');
+INSERT INTO Course (CID, C_Name) VALUES (101003, 'Databases');
+INSERT INTO Course (CID, C_Name) VALUES (101004, 'Websites');
+INSERT INTO Course (CID, C_Name) VALUES (101005, 'IS Management');
+GO
+
+INSERT INTO Course_Grades (CGID, Semester, CID_CG, SID_CG, Grade) VALUES (2010101,
+'SP10', 101005, 34567, 'D+');
+INSERT INTO Course_Grades (CGID, Semester, CID_CG, SID_CG, Grade) VALUES (2010308,
+'FA10', 101005, 34567, 'A-');
+INSERT INTO Course_Grades (CGID, Semester, CID_CG, SID_CG, Grade) VALUES (2010309,
+'FA10', 101001, 45678, 'B+');
+INSERT INTO Course_Grades (CGID, Semester, CID_CG, SID_CG, Grade) VALUES (2011308,
+'FA11', 101003, 23456, 'B-');
+INSERT INTO Course_Grades (CGID, Semester, CID_CG, SID_CG, Grade) VALUES (2012206,
+'SU12', 101002, 56789, 'A+');
+GO
+
+ALTER TABLE Student
+ALTER COLUMN S_FName VARCHAR(30) NOT NULL;
+GO
+
+ALTER TABLE Course
+ADD Faculty_LName VARCHAR(30) DEFAULT 'TBD' NOT NULL;
+GO
+
+UPDATE Course
+SET Faculty_LName= 'Potter', C_Name= 'Intro to Wizardry'
+WHERE CID = 101001;
+go
+
+EXEC sp_rename 'Course.C_Name', 'Course_Name'
+GO
+
+DELETE FROM Course
+	WHERE CID = 101004
+GO
+
+DELETE FROM Course
+	WHERE Course_Name = 'Websites'
+GO
+
+--Khong xoa duoc table Student
+DROP TABLE Student
+GO
+
+--Khong xoa duoc table Student
+TRUNCATE TABLE Course
+GO
+
+ALTER TABLE Course_Grades DROP CONSTRAINT CID_CG_fk
+GO
+
+ALTER TABLE Course_Grades 
+	ADD CONSTRAINT CID_CG_FK FOREIGN KEY (CID_CG) REFERENCES Course(CID) ON DELETE CASCADE
+GO
+
+ALTER TABLE Course_Grades DROP INDEX CID_CG_fk;
+ALTER TABLE Course_Grades DROP FOREIGN KEY SID_CG_fk;
+GO
+
